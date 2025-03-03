17e57ab4f63f5974cab9ea5fc9d87840658c0919
Root <katya.volodina46@gmail.com>
2025-03-03 16:41:35 +0400

    js-2: ввод кода

---
+++ js-2/script.js
@@ -0,0 +1,62 @@
+let inputGroup = document.querySelector('.input-group');
+let inputs = inputGroup.querySelectorAll('.code-number');
+let form = document.querySelector('.code-form');
+
+function focusFirstEmpty(event) {
+  event.preventDefault();
+
+  for (let input of inputs) {
+    if (!input.value) {
+      input.focus();
+      break;
+    }
+  }
+
+  let allFilled = [...inputs].every(input => input.value);
+
+  if (allFilled) {
+    inputs[inputs.length - 1].focus();
+  }
+}
+
+function focusToNext(event) {
+  let input = event.target;
+  let next = input.nextElementSibling;
+
+  if (next && next.tagName !== 'INPUT') {
+    next = next.nextElementSibling;
+  }
+
+  if (next) {
+    next.focus();
+  }
+}
+
+function focusToPrevious(event) {
+  let input = event.target;
+
+  if (event.key === "Backspace" && !input.value) {
+    let prev = input.previousElementSibling;
+    
+    if (prev && prev.tagName !== 'INPUT') {
+      prev = prev.previousElementSibling;
+    }
+
+    if (prev) {
+      prev.value = "";
+      prev.focus();
+    }
+  }
+}
+
+function alertValue(event) {
+  event.preventDefault();
+
+  let code = Array.from(inputs).map(input => input.value).join("");
+  alert(`Value is ${code}`);
+}
+
+inputGroup.addEventListener("mousedown", focusFirstEmpty);
+inputGroup.addEventListener("input", focusToNext);
+inputGroup.addEventListener("keydown", focusToPrevious);
+form.addEventListener("submit", alertValue);
